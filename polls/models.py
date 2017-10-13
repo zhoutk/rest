f0c24d511924d4d024c423bad21164c6ea0732b3
zhoutk <zhoutk@189.cn>
2017-10-13 09:52:55 +0800

    polls base ok.

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    create_time = models.DateTimeField('date when creating')
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
